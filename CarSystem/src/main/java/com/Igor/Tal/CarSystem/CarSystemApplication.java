0e6438ddfd69c795b0c48338fde9d0d840f3e092
igv7 <46227257+igv7@users.noreply.github.com>
2020-05-08 23:45:32 +0300

    Adding few annotations and Sysout "Start"

--- CarSystem/src/main/java/com/Igor/Tal/CarSystem/CarSystemApplication.java
+++ CarSystem/src/main/java/com/Igor/Tal/CarSystem/CarSystemApplication.java
@@ -2,12 +2,18 @@ package com.Igor.Tal.CarSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableMongoRepositories
+//@EnableWebSecurity
 public class CarSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarSystemApplication.class, args);
+		System.out.println("Start");
 	}
 
 }
