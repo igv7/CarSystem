0d99f4244945599525aa60d441862db98396d4bf
igv7 <46227257+igv7@users.noreply.github.com>
2020-06-04 22:58:06 +0300

    Added signUp service and controller

---
+++ CarSystem/src/main/java/com/Igor/Tal/CarSystem/service/SignUpService.java
@@ -0,0 +1,10 @@
+package com.Igor.Tal.CarSystem.service;
+
+import com.Igor.Tal.CarSystem.model.Client;
+
+public interface SignUpService {
+	
+	//Sign Up
+	public Client signUp(Client client) throws Exception;
+
+}
