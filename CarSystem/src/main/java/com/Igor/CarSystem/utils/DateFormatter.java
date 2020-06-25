329834132ed236f4906024d66e013d6e1ed27c42
igv7 <46227257+igv7@users.noreply.github.com>
2020-06-25 04:17:23 +0300

    Updated DateTimeFormatter

--- CarSystem/src/main/java/com/Igor/CarSystem/utils/DateFormatter.java
+++ CarSystem/src/main/java/com/Igor/CarSystem/utils/DateFormatter.java
@@ -8,7 +8,8 @@ public class DateFormatter {
 	public static String getCurrentDate() {
 		LocalDateTime dateTime = LocalDateTime.now();
 		
-		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy_HH:mm:ss");
+//		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy_HH:mm:ss");
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 		
 		String formattedDate = dateTime.format(dateTimeFormatter);
 		return formattedDate;
